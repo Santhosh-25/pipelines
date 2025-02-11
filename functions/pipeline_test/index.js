01c5432208186bc55d11d40150acfafc14db7239
SanthoshCRM-25 <santhosh.mahendiran+crm@zohotest.com>
2025-02-12 00:13:32 +0530

    fourth commit

--- functions/pipeline_test/index.js
+++ functions/pipeline_test/index.js
@@ -1,24 +1,25 @@
-'use strict';
+"use strict";
 
 const { IncomingMessage, ServerResponse } = require("http");
 
 /**
- * 
- * @param {IncomingMessage} req 
- * @param {ServerResponse} res 
+ *
+ * @param {IncomingMessage} req
+ * @param {ServerResponse} res
  */
 module.exports = (req, res) => {
-	var url = req.url;
+  var url = req.url;
 
-	switch (url) {
-		case '/':
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write('<h1>Hello from index.js<h1>');
-			break;
-		default:
-			res.writeHead(404);
-			res.write('You might find the page you are looking for at "/" path');
-			break;
-	}
-	res.end();
+  switch (url) {
+    case "/":
+      console.log("test logger");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Hello from index.js<h1>");
+      break;
+    default:
+      res.writeHead(404);
+      res.write('You might find the page you are looking for at "/" path');
+      break;
+  }
+  res.end();
 };
